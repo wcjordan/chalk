87005692aa92be84611013b7a66b85972d041cb5
Chris Jordan <chris.jordan@schrodinger.com>
2020-09-11 19:03:03 -0600

    Use Redux ConnectedProps for typing

--- ui/js/src/App.tsx
+++ ui/js/src/App.tsx
@@ -1,13 +1,13 @@
 import _ from 'lodash';
-import { connect } from 'react-redux';
+import { connect, ConnectedProps } from 'react-redux';
 import React from 'react';
 import AddTodo from './components/AddTodo';
-import { Todo, ReduxState } from './redux/types';
+import { ReduxState } from './redux/types';
 import TodoItem from './components/TodoItem';
 import { createTodo } from './redux/reducers';
 import './App.css';
 
-export function App(props: Props) {
+export function App(props: ConnectedProps<typeof connector>) {
   const { todos } = props;
   const todoViews = _.map(todos, todo => (
     <TodoItem key={todo.id || ''} todo={todo} />
@@ -20,15 +20,11 @@ export function App(props: Props) {
   );
 }
 
-type Props = {
-  createTodo: Function;
-  todos: Todo[];
-};
-
 const mapStateToProps = (state: ReduxState) => {
   return {
     todos: state.todosApi.entries,
   };
 };
 const mapDispatchToProps = { createTodo };
-export default connect(mapStateToProps, mapDispatchToProps)(App);
+const connector = connect(mapStateToProps, mapDispatchToProps);
+export default connector(App);
