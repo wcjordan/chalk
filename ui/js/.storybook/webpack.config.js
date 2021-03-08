f41e4435ac7384bd57517fdf3c2f3bc12ea11832
Chris Jordan <chris.jordan@schrodinger.com>
2021-03-08 18:58:57 -0500

    fix Storybook when running outside Docker

---
+++ ui/js/.storybook/webpack.config.js
@@ -0,0 +1,43 @@
+const path = require('path');
+
+const externalLibs = [path.resolve(__dirname, '../node_modules/react-native')];
+
+module.exports = ({ config }) => {
+  config.module.rules.push({
+    test: /\.tsx?$/,
+    exclude: /node_modules/,
+    use: [
+      {
+        loader: 'babel-loader',
+        options: {
+          presets: ['babel-preset-expo'],
+        },
+      },
+    ],
+  });
+  config.module.rules.push({
+    test: /\.jsx?$/,
+    include: externalLibs,
+    use: [
+      {
+        loader: 'babel-loader',
+        options: {
+          presets: ['babel-preset-expo'],
+        },
+      },
+    ],
+  });
+
+  config.resolve.alias = {
+    // replace `react-native` imports with `react-native-web`
+    'react-native$': require.resolve('react-native-web'),
+    'react-native-svg': require.resolve('react-native-svg-web'),
+    '../Utilities/Platform$': require.resolve(
+      'react-native-web/dist/exports/Platform',
+    ),
+  };
+
+  config.resolve.extensions.push('.ts', '.tsx', '.web.js', '.web.tsx');
+
+  return config;
+};
