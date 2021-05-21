be457336163f996b9acd815df003361cbb1550b4
Chris Jordan <chris.jordan@schrodinger.com>
2021-05-21 14:29:57 -0400

    Get storybook working and fix snapshot tests

--- ui/js/.storybook/webpack.config.js
+++ ui/js/.storybook/webpack.config.js
@@ -1,43 +1,5 @@
-const path = require('path');
-
-const externalLibs = [path.resolve(__dirname, '../node_modules/react-native')];
+const { withUnimodules } = require('@expo/webpack-config/addons');
 
 module.exports = ({ config }) => {
-  config.module.rules.push({
-    test: /\.tsx?$/,
-    exclude: /node_modules/,
-    use: [
-      {
-        loader: 'babel-loader',
-        options: {
-          presets: ['babel-preset-expo'],
-        },
-      },
-    ],
-  });
-  config.module.rules.push({
-    test: /\.jsx?$/,
-    include: externalLibs,
-    use: [
-      {
-        loader: 'babel-loader',
-        options: {
-          presets: ['babel-preset-expo'],
-        },
-      },
-    ],
-  });
-
-  config.resolve.alias = {
-    // replace `react-native` imports with `react-native-web`
-    'react-native$': require.resolve('react-native-web'),
-    'react-native-svg': require.resolve('react-native-svg-web'),
-    '../Utilities/Platform$': require.resolve(
-      'react-native-web/dist/exports/Platform',
-    ),
-  };
-
-  config.resolve.extensions.push('.ts', '.tsx', '.web.js', '.web.tsx');
-
-  return config;
+  return withUnimodules(config);
 };
