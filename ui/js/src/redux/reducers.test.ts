30a7f1d1b0b59d1054e2564024a20c1caca9406b
Chris Jordan <chris.jordan@schrodinger.com>
2022-01-13 00:09:20 -0500

    Add a few more tests

--- ui/js/src/redux/reducers.test.ts
+++ ui/js/src/redux/reducers.test.ts
@@ -2,8 +2,13 @@ import '../__mocks__/matchMediaMock';
 import configureMockStore from 'redux-mock-store';
 import fetchMock from 'fetch-mock-jest';
 import thunk from 'redux-thunk';
-import reducers, { updateTodo } from './reducers';
+import reducers, {
+  completeAuthentication,
+  updateTodo,
+  updateTodoLabels,
+} from './reducers';
 import { getTodosApi } from './todosApiSlice';
+import { getWsRoot } from './fetchApi';
 
 const mockStore = configureMockStore([thunk]);
 
@@ -55,6 +60,93 @@ describe('updateTodo', function () {
   });
 });
 
+describe('updateTodoLabels', function () {
+  afterEach(function () {
+    fetchMock.restore();
+  });
+
+  it('should make a PATCH request and dispatch actions', async function () {
+    const todoId = 1;
+    const newLabels = ['new', 'labels'];
+    const payload = {
+      id: todoId,
+      labels: newLabels,
+    };
+    fetchMock.patchOnce(`${getTodosApi()}${todoId}/`, {
+      body: payload,
+    });
+
+    const store = mockStore({
+      workspace: {
+        labelTodoId: todoId,
+      },
+    });
+    await store.dispatch(updateTodoLabels(newLabels));
+
+    const actions = store.getActions();
+    expect(actions.length).toEqual(2);
+
+    // Verify the pending handler is called based on the patch argument
+    const pendingAction = actions[0];
+    expect(pendingAction.meta.arg).toEqual(payload);
+    expect(pendingAction.type).toEqual('todosApi/update/pending');
+
+    // Verify the fulfilled handler is called with the returned todo
+    const fulfilledAction = actions[1];
+    expect(fulfilledAction.meta.arg).toEqual(payload);
+    expect(fulfilledAction.payload).toEqual(payload);
+    expect(fulfilledAction.type).toEqual('todosApi/update/fulfilled');
+
+    // Verify we make the server request
+    expect(fetchMock).toBeDone();
+  });
+
+  it('should error if no Todo is being labeled', async function () {
+    expect.assertions(1);
+
+    const store = mockStore({
+      workspace: {
+        labelTodoId: null,
+      },
+    });
+    await expect(
+      async () => await store.dispatch(updateTodoLabels([])),
+    ).rejects.toThrow('Unable to edit a todo w/ null ID');
+  });
+});
+
+describe('completeAuthentication', function () {
+  it('should make a request to auth_callback with the auth token', async function () {
+    const token = 'token';
+
+    fetchMock.getOnce(`${getWsRoot()}api/todos/auth_callback/?code=${token}`, {
+      body: 'Logged In!',
+      status: 200,
+      headers: {
+        'set-cookie':
+          'csrftoken=CSRFToken; Max-Age=31449600; Path=/; SameSite=Lax',
+      },
+    });
+
+    const store = mockStore();
+    await store.dispatch(completeAuthentication(token));
+
+    const actions = store.getActions();
+    expect(actions.length).toEqual(1);
+
+    // Verify the pending handler is called based on the patch argument
+    const pendingAction = actions[0];
+    expect(pendingAction.type).toEqual('workspace/logIn');
+    expect(pendingAction.payload).toEqual({
+      loggedIn: true,
+      csrfToken: 'CSRFToken',
+    });
+
+    // Verify we make the server request
+    expect(fetchMock).toBeDone();
+  });
+});
+
 describe('workspace reducer', function () {
   it('should return the initial state', function () {
     expect(reducers.workspace(undefined, {})).toEqual({
@@ -63,6 +155,55 @@ describe('workspace reducer', function () {
       filterLabels: [],
       labelTodoId: null,
       loggedIn: false,
+    });
+  });
+
+  describe('workspace/filterByLabels', function () {
+    it('should update the filterLabels', function () {
+      const result = reducers.workspace(
+        {
+          filterLabels: ['old'],
+        },
+        {
+          type: 'workspace/filterByLabels',
+          payload: ['new', 'otherNew'],
+        },
+      );
+      expect(result).toEqual({
+        filterLabels: ['new', 'otherNew'],
+      });
+    });
+  });
+
+  describe('workspace/setTodoLabelingId', function () {
+    it('should update the labeling id', function () {
+      const result = reducers.workspace(
+        {
+          labelTodoId: 1,
+        },
+        {
+          type: 'workspace/setTodoLabelingId',
+          payload: 2,
+        },
+      );
+      expect(result).toEqual({
+        labelTodoId: 2,
+      });
+    });
+
+    it('should support cancelling a labeling', function () {
+      const result = reducers.workspace(
+        {
+          labelTodoId: 1,
+        },
+        {
+          type: 'workspace/setTodoLabelingId',
+          payload: null,
+        },
+      );
+      expect(result).toEqual({
+        labelTodoId: null,
+      });
     });
   });
 
