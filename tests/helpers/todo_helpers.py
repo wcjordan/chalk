69282dbdfcf7a048d7ee8b353bb93e48746b788b
Chris Jordan <chris.jordan@schrodinger.com>
2022-03-05 00:37:36 -0500

    Remove selenium imports

--- tests/helpers/todo_helpers.py
+++ tests/helpers/todo_helpers.py
@@ -1,9 +1,3 @@
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.support.ui import WebDriverWait
-
-
 CHECKED_ICON_TEXT = '󰄲'
 DELETE_ICON_TEXT = '󰧧'
 LABELS_ICON_TEXT = '󰜢'
