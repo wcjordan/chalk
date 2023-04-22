026bda0eb3260b2da932a582901180081f0dd9e4
Chris Jordan <chris.jordan@schrodinger.com>
2023-04-22 17:52:49 -0400

    Fix Playwright tests

--- tests/label_persistence_test.py
+++ tests/label_persistence_test.py
@@ -1,10 +1,13 @@
 import pytest
 
-from helpers.label_helpers import add_todo_w_labels
+from helpers.label_helpers import (add_todo_w_labels, toggle_label_filter)
 
 
 @pytest.mark.parametrize('test_name', ['Label: Label Persistence'])
 def test_label_persistence(page, todo_prefix):
+    # Remove default filter
+    toggle_label_filter(page, 'Unlabeled')
+
     # Add todo w/ 5 min & errand labels
     todo_description = f'{todo_prefix} test todo'
     labels = ['errand', '5 minutes']
