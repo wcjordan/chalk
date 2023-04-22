8e7115618ddcc8229c282125747b1425ed8146a8
Chris Jordan <flipperkid@gmail.com>
2023-04-22 16:00:25 -0400

    Storybook 7 migration (#70)

--- ui/js/.storybook/webpack.config.js
+++ ui/js/.storybook/webpack.config.js
@@ -1,5 +1,44 @@
-const { withUnimodules } = require('@expo/webpack-config/addons');
+import path from 'path';
 
 module.exports = ({ config }) => {
-  return withUnimodules(config);
+  config.module.rules.push({
+    test: /LICENSE$/,
+    include: /node_modules/,
+    loader: 'file-loader',
+  });
+  config.module.rules.push({
+    test: /.map$/,
+    include: /node_modules/,
+    loader: 'file-loader',
+  });
+
+  // Disable useDataLoader hook so Storybook doesn't try to hit the backend
+  config.resolve.alias['expo-constants'] = require.resolve(
+    '../__mocks__/expo-constants.js',
+  );
+
+  // Mock out useAuthRequest for login button
+  config.resolve.alias['expo-auth-session/providers/google'] = require.resolve(
+    '../__mocks__/expo-auth-session/providers/google.js',
+  );
+
+  // Swap Expo icons for react-native-vector-icons
+  config.resolve.alias['react-native-vector-icons/MaterialCommunityIcons'] =
+    require.resolve('@expo/vector-icons/MaterialCommunityIcons');
+
+  config.resolve.fallback = {
+    fs: false,
+    constants: false,
+    child_process: false,
+    crypto: false,
+    module: false,
+    net: false,
+    os: false,
+    path: false,
+    stream: false,
+    v8: false,
+    worker_threads: false,
+  };
+
+  return config;
 };
--- ui/js/src/components/LabelPicker.stories.tsx
+++ ui/js/src/components/LabelPicker.stories.tsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { StyleSheet, View } from 'react-native';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 import LabelPicker from './LabelPicker';
 
 const labels = [
@@ -24,10 +26,27 @@ const selectedLabels = {
   mobile: true,
 };
 
+const styles = StyleSheet.create({
+  wrapper: {
+    paddingTop: '100px',
+    padddingBottom: '100px',
+  },
+});
+const wrapper = (labelPicker) => (
+  <SafeAreaProvider>
+    <View style={styles.wrapper}>{labelPicker}</View>
+  </SafeAreaProvider>
+);
+
 export default {
   title: 'Label Picker',
   component: LabelPicker,
 };
-export const DefaultLabelPicker: React.FC = () => (
-  <LabelPicker labels={labels} selectedLabels={selectedLabels} visible={true} />
-);
+export const DefaultLabelPicker: React.FC = () =>
+  wrapper(
+    <LabelPicker
+      labels={labels}
+      selectedLabels={selectedLabels}
+      visible={true}
+    />,
+  );
