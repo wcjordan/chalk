e962750fc87286261723b89c06eedfd29b02c96c
Chris Jordan <chris.jordan@schrodinger.com>
2023-09-17 13:09:25 -0400

    Fix work context test

--- tests/helpers/work_context_helpers.py
+++ tests/helpers/work_context_helpers.py
@@ -2,9 +2,12 @@ def get_active_work_context(page):
     active_chip = page.locator('[data-testid="work-context-filter"] [data-testid="chip-active"]')
     if not active_chip:
         return None
-    return active_chip.allTextContents()
+
+    full_text = active_chip.inner_text()
+    # Remove the leading icon
+    return full_text.split('\n')[-1]
 
 
 def select_work_context(page, work_context):
-    work_context_chip = page.locator(f'[data-testid="work-context-filter"] :text-is("{work_context}')
+    work_context_chip = page.locator(f'[data-testid="work-context-filter"] :text-is("{work_context}")')
     work_context_chip.click()
--- tests/work_contexts_test.py
+++ tests/work_contexts_test.py
@@ -1,6 +1,6 @@
 import pytest
 
-from helpers.label_helpers import (add_labels, dismiss_add_label_modal, get_todo_labels)
+from helpers.label_helpers import (add_labels, add_todo_w_labels, dismiss_add_label_modal, get_todo_labels)
 from helpers.todo_helpers import (add_todo, find_todos, list_todo_descriptions, wait_for_todo,
                                   wait_for_todo_to_disappear)
 from helpers.work_context_helpers import (get_active_work_context, select_work_context)
@@ -38,7 +38,7 @@ def test_work_contexts(page, todo_prefix):
     # Verify the todo is visible and label is added
     todos = find_todos(page, quick_todo_description)
     assert todos.count() == 1
-    assert get_todo_labels(todos.first) == ['up next', '5 minutes']
+    assert get_todo_labels(todos.first) == ['5 minutes', 'up next']
 
     # Dismiss picker modal
     dismiss_add_label_modal(page)
@@ -50,7 +50,7 @@ def test_work_contexts(page, todo_prefix):
 
     # Add todos for shopping, urgent, & Chalk planning
     shopping_todo_description = f'{todo_prefix} buy milk'
-    add_todo_w_labels(page, shopping_todo_description, ['shopping'])
+    add_todo_w_labels(page, shopping_todo_description, ['Shopping'])
     urgent_todo_description = f'{todo_prefix} pay bills'
     add_todo_w_labels(page, urgent_todo_description, ['urgent'])
     planning_todo_description = f'{todo_prefix} investigate technology'
@@ -70,6 +70,8 @@ def test_work_contexts(page, todo_prefix):
 def _verify_todo_in_work_context(page, todo_prefix, description, work_context):
     select_work_context(page, work_context)
     wait_for_todo(page, description)
+
+    assert get_active_work_context(page) == work_context
     todos = find_todos(page, todo_prefix, partial=True)
     assert todos.count() == 1
     assert list_todo_descriptions(page, todo_prefix) == [description]
