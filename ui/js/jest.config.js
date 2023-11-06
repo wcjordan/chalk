0e1510cbaa4feb1318f048fece8b590fef12d1f4
Chris Jordan <chris.jordan@schrodinger.com>
2023-11-05 21:25:30 -0500

    Publish a junit report for jest tests

--- ui/js/jest.config.js
+++ ui/js/jest.config.js
@@ -2,6 +2,7 @@ module.exports = {
   preset: 'jest-expo/android',
   testEnvironment: 'jsdom',
 
+  reporters: ['default', 'jest-junit'],
   setupFiles: [
     './node_modules/@react-native-google-signin/google-signin/jest/build/setup.js',
   ],
