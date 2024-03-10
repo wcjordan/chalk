acb613275d8ed9073e10869cc2108d4d47929a53
Chris Jordan <chris.jordan@schrodinger.com>
2024-03-10 14:00:09 -0400

    Add redux decorator to Storybook

--- ui/js/.storybook/preview.tsx
+++ ui/js/.storybook/preview.tsx
@@ -1,6 +1,18 @@
+import React from 'react';
 import type { Preview } from '@storybook/react';
+import { Provider } from 'react-redux';
+import { setupStore } from '../src/redux/store';
+
+const reduxDecorator = (story) => (
+  <Provider store={setupStore()}>
+    { story() }
+  </Provider>
+);
 
 const preview: Preview = {
+  decorators: [
+    reduxDecorator,
+  ],
   parameters: {
     actions: { argTypesRegex: '^on[A-Z].*' },
     controls: {
