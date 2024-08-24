346ec264ef817009b86f1714010ca9f295d9c574
Chris Jordan <chris.jordan@schrodinger.com>
2024-08-24 10:29:14 -0400

    Generate snapshots

---
+++ ui/js/.storybook/test-runner.ts
@@ -0,0 +1,12 @@
+import type { TestRunnerConfig } from '@storybook/test-runner';
+
+const config: TestRunnerConfig = {
+  async postVisit(page, context) {
+    const elementHandler = await page.$('#storybook-root');
+    const innerHTML = await elementHandler.innerHTML();
+    expect(innerHTML).toMatchSnapshot();
+  },
+};
+
+export default config;
+
