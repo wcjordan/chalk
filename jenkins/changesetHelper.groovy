38c60effc3f1a2e142f9494060859552a6905f64
Chris Jordan <flipperkid@gmail.com>
2025-06-25 22:19:16 -0400

    Debugging

--- jenkins/changesetHelper.groovy
+++ jenkins/changesetHelper.groovy
@@ -2,6 +2,10 @@
 def getChangeSetToTest() {
     // Fetch main so that it's available to diff against
     sh 'git fetch origin refs/heads/main:refs/remotes/origin/main'
+    sh 'git branch -a'
+    sh 'git status'
+    sh 'git diff --name-only origin/main...'
+
     def changeSet = sh (
         script: 'git diff --name-only origin/main...',
         returnStdout: true
