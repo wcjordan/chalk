fdd08e098bb1a563c22aac7dcf137dcb2240084a
Chris Jordan <flipperkid@gmail.com>
2025-09-07 23:39:23 -0400

    Fix test mode for storybook

--- ui/js/.storybook/preview.tsx
+++ ui/js/.storybook/preview.tsx
@@ -2,6 +2,8 @@ import type { Preview } from '@storybook/react';
 import { DefaultTheme, Provider as PaperProvider } from 'react-native-paper';
 import MaterialCommunityIcon from '../__mocks__/MaterialCommunityIcon';
 
+window.IS_STORYBOOK = true;
+
 const preview: Preview = {
   parameters: {
     controls: {
--- ui/js/src/helpers.ts
+++ ui/js/src/helpers.ts
@@ -1,6 +1,8 @@
 import Constants from 'expo-constants';
 import { Platform } from 'react-native';
 
+declare const window: any; // eslint-disable-line @typescript-eslint/no-explicit-any
+
 export const getEnvFlags = () => {
   const flags = Constants.expoConfig?.extra || {};
 
@@ -8,6 +10,9 @@ export const getEnvFlags = () => {
   // This is because we wan't to build a single container w/ prod artifacts
   // and use it for the dev & ci use cases that don't use the Expo web server
   if (Platform.OS === 'web' && flags.ENVIRONMENT !== 'test') {
+    if (window.IS_STORYBOOK === true) {
+      flags.ENVIRONMENT = 'test';
+    }
     const hostname = document.location.hostname;
     if (hostname.startsWith('localhost') || hostname.startsWith('chalk-dev')) {
       flags.ENVIRONMENT = 'DEV';
