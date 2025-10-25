1060ea545894b93c816c72c5efc4e356e7718765
Chris Jordan <flipperkid@gmail.com>
2025-10-25 13:49:24 -0400

    If no test_id on dom node, walk up to find the nearest parent with a test id

--- test_gen/feature_extraction/feature_viz.py
+++ test_gen/feature_extraction/feature_viz.py
@@ -23,6 +23,14 @@ def print_w_color(text: str, color_code: str):
     print(f"{color_code}{text}{bcolors.ENDC}")
 
 
+def create_dom_path_to_node_dict(dom_nodes: dict) -> dict:
+    """Create a mapping from DOM paths to nodes."""
+    dom_path_to_node = {}
+    for node in dom_nodes.values():
+        dom_path_to_node[node.dom_path] = node
+    return dom_path_to_node
+
+
 def _extract_input_value(interaction: UserInteraction) -> dict:
     """Extract the input value helper."""
     return interaction.value.get("value")
@@ -46,31 +54,55 @@ def _is_same_input_interaction(
     return _extract_input_value(last_input) in _extract_input_value(next_input)
 
 
-def extract_dom_node_name(dom_nodes: dict, target_id: str) -> str:
+def extract_dom_node_name(
+    dom_nodes: dict, dom_path_to_node: dict, target_id: str
+) -> str:
     """Extract the DOM node name."""
     dom_node = dom_nodes.get(target_id)
     if dom_node.data_testid:
         return f'{dom_node.tag} w/ test_id="{dom_node.data_testid}"'
+
+    if dom_node.dom_path is None:
+        return str(dom_node)
+
+    # Find a parent with a data-testid if possible
+    rest = []
+    dom_path = dom_node.dom_path
+    while " > " in dom_path:
+        path_split = dom_path.rsplit(" > ", 1)
+        parent_path = path_split[0].strip()
+        rest.insert(0, path_split[-1].strip())
+        parent_node = dom_path_to_node.get(parent_path)
+
+        if parent_node and parent_node.data_testid:
+            rest_str = " > ".join(rest)
+            return f'{dom_node.tag} w/ test_id="{parent_node.data_testid}" > {rest_str}'
+
+        dom_path = parent_path
+    print_w_color(f"Unknown dom node: {dom_node}", bcolors.RED)
     return str(dom_node)
 
 
-def print_input_interaction(dom_nodes: dict, interaction: dict):
+def print_input_interaction(dom_node_name: str, interaction: dict):
     """Print the input interaction information."""
-    dom_node_name = extract_dom_node_name(dom_nodes, interaction.target_id)
     print_w_color(
         f'>   Typed into {dom_node_name}\n      "{_extract_input_value(interaction)}"',
         bcolors.CYAN,
     )
 
 
-def print_interaction(dom_nodes, interaction):
+def print_interaction(
+    dom_nodes: dict, dom_path_to_node: dict, interaction: UserInteraction
+):
     """Print a completed interaction."""
     if interaction is None:
         return
 
-    dom_node_name = extract_dom_node_name(dom_nodes, interaction.target_id)
+    dom_node_name = extract_dom_node_name(
+        dom_nodes, dom_path_to_node, interaction.target_id
+    )
     if interaction.action == "input":
-        print_input_interaction(dom_nodes, interaction)
+        print_input_interaction(dom_node_name, interaction)
     elif interaction.action == "click":
         print_w_color(f">   Clicked on {dom_node_name}", bcolors.GREEN)
     elif interaction.action == "scroll":
@@ -91,8 +123,8 @@ def main():
     chunk_files = list(Path(INPUT_PATH).glob("*.json"))
     total_files = len(chunk_files)
     for chunk_file in sorted(chunk_files):
-        print(
-            f"\n{bcolors.PINK}=== Interaction Sequence for {chunk_file.name} ==={bcolors.ENDC}"
+        print_w_color(
+            f"\n=== Interaction Sequence for {chunk_file.name} ===", bcolors.PINK
         )
 
         # Load and parse the chunk JSON
@@ -105,6 +137,7 @@ def main():
             sys.exit(1)
         feature_chunk = feature_chunk_from_dict(chunk_data)
         dom_nodes = feature_chunk.features.get("ui_nodes", {})
+        dom_path_to_node = create_dom_path_to_node_dict(dom_nodes)
 
         last_interaction = None
         for next_interaction in feature_chunk.features.get("interactions", []):
@@ -114,11 +147,11 @@ def main():
                 last_interaction = next_interaction
                 continue
 
-            print_interaction(dom_nodes, last_interaction)
+            print_interaction(dom_nodes, dom_path_to_node, last_interaction)
             last_interaction = next_interaction
 
         if last_interaction is not None:
-            print_interaction(dom_nodes, last_interaction)
+            print_interaction(dom_nodes, dom_path_to_node, last_interaction)
     print(f"Printed {total_files} feature chunk files.")
 
 
